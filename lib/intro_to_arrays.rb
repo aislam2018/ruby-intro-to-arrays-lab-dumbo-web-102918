12ef3371af2547dfb9f6e856a5a85cff95b1a05e
anik1606 <anik1606@gmail.com>
2018-10-10 20:13:42 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -32,3 +32,4 @@ def third_element(my_third_element)
     return last_country
   end
   def length_of_array(length)
+    programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
