454d70de4f1067b3e7b311d949800fb51a6dd296
anik1606 <anik1606@gmail.com>
2018-10-10 20:07:40 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,23 @@
+def instantiate_new_array
+  my_new_array = []
+  return my_new_array
+end
+def array_with_two_elements
+  chars = ["a","b"]
+  return chars
+end
+def first_element(my_first_element)
+  taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = taylor_swift[0]
+  return my_first_element
+end
+def third_element(my_third_element)
+   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+   my_third_element = taylor_swift[2]
+   return my_third_element
+ end
+ def last_element(my_last_element)
+    taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+    my_last_element = taylor_swift[-1]
+    return my_last_element
+  end
